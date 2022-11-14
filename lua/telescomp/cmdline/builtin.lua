917754b50815912cfad8339d4657520c1628f022
atusy <30277794+atusy@users.noreply.github.com>
2022-11-14 22:09:40 +0900

    add telescomp.cmdline.builtin.cmdline

--- lua/telescomp/cmdline/builtin.lua
+++ lua/telescomp/cmdline/builtin.lua
@@ -1,5 +1,8 @@
 local fn = vim.fn
+local startswith = vim.startswith
+local tbl_map = vim.tbl_map
 local cmdline = require('telescomp.cmdline')
+local utils = require('telescomp.utils')
 
 local M = {}
 
@@ -14,6 +17,54 @@ M.git_ref = cmdline.create_completer({
     end
   }
 })
+
+local function getcompletion(opts_comp)
+  local left = opts_comp.left
+  local default_text = opts_comp.default_text
+  local completion = fn.getcompletion(left .. default_text, 'cmdline')
+
+  if startswith(left, 'set') and startswith(default_text, 'no') then
+    return tbl_map(function(x) return 'no' .. x end, completion)
+  end
+
+  return completion
+end
+
+local function clean_pattern(x, y)
+  if y == nil then return '' end
+  local n = fn.strchars(x)
+  if n == 0 then return x end
+  for i = 0, n - 1 do
+    if startswith(y, fn.strcharpart(x, i, n)) then
+      return fn.strcharpart(x, 0, i)
+    end
+  end
+  return x
+end
+
+local _complete = cmdline.create_completer({
+  opts_picker = { finder = function() return { results = {} } end }
+})
+
+function M.cmdline(opts_picker, opts_comp)
+  -- setup opts_comp
+  opts_comp = cmdline.spec_completer_options(opts_comp)
+
+  -- find candidates and setup opts_picker
+  local results = getcompletion(opts_comp)
+  opts_picker = utils.merge(
+    { finder = require('telescope.finders').new_table({ results = results }) },
+    opts_picker
+  )
+
+  -- update opts_comp
+  if startswith(opts_comp.left, 'lua ') then
+    opts_comp.left = opts_comp.left .. clean_pattern(opts_comp.default_text, results[1])
+  end
+  opts_comp.default_text = ''
+
+  _complete(opts_picker, opts_comp)
+end
 
 M.find_files = cmdline.create_completer({
   picker = require('telescope.builtin').find_files
